b754d37bf1d0a595a3af34761ac0d34b70e5f710
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-04-05 23:37:22 +0300

    .

---
+++ js/hw-17.js
@@ -0,0 +1,55 @@
+//№1
+
+
+// const inputField = document.getElementById('inputText');
+// const changeButton = document.getElementById('changeButton');
+
+// changeButton.addEventListener('click', function() {
+//     const newText = inputField.value;
+//     changeButton.textContent = newText;
+// });
+
+
+
+
+//№2
+
+// const image = document.getElementById('mainImage');
+// const changeButton = document.getElementById('changeButton');
+// changeButton.addEventListener('click', function() {
+//     image.src = '/Users/marinaukrainec/Documents/H.W-DOM/js/flower.jpeg'
+// })
+
+//я не можу вставити ніяк тут посилання, 
+//у мене вроді все получилось просто воно не замінює 
+//на іншу картинку через те, що я не можу розібратись з src 
+
+
+
+
+
+//№3
+
+// const link = document.getElementById('myLink');
+// const image = document.getElementById('myImage');
+// const button = document.getElementById('changeAttributesButton');
+
+// button.addEventListener('click', function() {
+
+//     link.href = 'https://www.tiktok.com/uk-UA/';
+
+//     image.alt = 'квіточка семецвіт';
+// });
+
+
+
+
+//№4
+
+const list = document.getElementById('myList');
+const firstListItem = list.firstElementChild;
+const button = document.getElementById('changeListItemButton');
+
+button.addEventListener('click', function() {
+    firstListItem.textContent = 'Новий текст для першого елемента';
+});
